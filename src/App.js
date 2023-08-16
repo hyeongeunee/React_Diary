c552a4e27be34b8d9b0c78551f7c9d3e99002282
hyeongeun <hhg7899@gamil.com>
2023-08-16 09:49:45 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "현근",
+    content: "하위용",
+    emotion: 1,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "황현근",
+    content: "하위",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "박지용",
+    content: "안녕",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 4,
+    author: "김민규",
+    content: "안녕",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
