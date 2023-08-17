c2fee689769e32796ad3947112a508ebdd95331c
hyeongeun <hhg7899@gmail.com>
2023-08-17 10:54:39 +0900

    Diary item & list & editor update

--- src/App.js
+++ src/App.js
@@ -1,46 +1,107 @@
-import "./App.css";
-import React, { useState, useEffect } from "react";
-import DiaryEditor from "./DiaryEditor";
-import DiaryList from "./DiaryList";
-
-const dummyList = [
-  {
-    id: 1,
-    author: "현근",
-    content: "하위용",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "황현근",
-    content: "하위",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "박지용",
-    content: "안녕",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 4,
-    author: "김민규",
-    content: "안녕",
-    emotion: 4,
-    created_date: new Date().getTime(),
-  },
-];
+import './App.css';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+// import OptimizeTest from './OptimizeTest';
+// import Lifecycle from './Lifecycle';
+// const dummyList = [
+//   {
+//     id: 1,
+//     author: "현근",
+//     content: "하위용",
+//     emotion: 1,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 2,
+//     author: "황현근",
+//     content: "하위",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 3,
+//     author: "박지용",
+//     content: "안녕",
+//     emotion: 5,
+//     created_date: new Date().getTime(),
+//   },
+//   {
+//     id: 4,
+//     author: "김민규",
+//     content: "안녕",
+//     emotion: 4,
+//     created_date: new Date().getTime(),
+//   },
+// ];
 
 function App() {
-  return (
-    <div className="App">
-      <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    const dataId = useRef(0);
+
+    const getData = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments').then((res) => res.json());
+        const initData = res.slice(0, 20).map((it) => {
+            return {
+                author: it.email,
+                content: it.body,
+                emotion: Math.floor(Math.random() * 5) + 1,
+                created_date: new Date().getTime(),
+                id: dataId.current++,
+            };
+        });
+
+        setData(initData);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const onCreate = (author, content, emotion) => {
+        const created_date = new Date().getTime;
+        const newItem = {
+            author,
+            content,
+            emotion,
+            created_date,
+            id: dataId.current,
+        };
+        dataId.current += 1;
+        setData([newItem, ...data]);
+    };
+
+    const onRemove = (targetId) => {
+        const newDiaryList = data.filter((it) => it.id !== targetId);
+        setData(newDiaryList);
+    };
+
+    const onEdit = (targetId, newContent) => {
+        setData(data.map((it) => (it.id === targetId ? { ...it, content: newContent } : it)));
+    };
+
+    const getDiaryAnalysis = useMemo(() => {
+        const goodCount = data.filter((it) => it.emotion >= 3).length;
+        const badCount = data.length - goodCount;
+        const goodRatio = (goodCount / data.length) * 100;
+        return { goodCount, badCount, goodRatio };
+    }, [data.length]);
+
+    const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
+    return (
+        <div className="App">
+            {/* <OptimizeTest /> */}
+            {/* <Lifecycle /> */}
+            <DiaryEditor onCreate={onCreate} />
+            <div>전체 일기 : {data.length}</div>
+            <div>기분 좋은 일기 개수 : {goodCount}</div>
+            <div>기분 나쁜 일기 개수 : {badCount}</div>
+            <div>기분 좋은 일기 비율 : {goodRatio}</div>
+            <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
+        </div>
+    );
 }
 
 export default App;
--- src/DiaryList.jsx
+++ src/DiaryList.jsx
@@ -1,21 +1,21 @@
-import DiaryItem from "./DiaryItem.jsx";
+import DiaryItem from './DiaryItem.jsx';
 
-const DiaryList = ({ diaryList }) => {
-  return (
-    <div className="DiaryList">
-      <h2>일기 리스트</h2>
-      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
-      <div>
-        {diaryList.map((it) => (
-          <DiaryItem key={it.id} {...it} />
-        ))}
-      </div>
-    </div>
-  );
+const DiaryList = ({ onEdit, diaryList, onRemove }) => {
+    return (
+        <div className="DiaryList">
+            <h2>일기 리스트</h2>
+            <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {diaryList.map((it) => (
+                    <DiaryItem key={it.id} {...it} onEdit={onEdit} onRemove={onRemove} />
+                ))}
+            </div>
+        </div>
+    );
 };
 
 DiaryList.defaulutProps = {
-  diaryList: [],
+    diaryList: [],
 };
 
 export default DiaryList;
